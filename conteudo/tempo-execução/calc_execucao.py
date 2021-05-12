79469592b59e34b9f959d6c8f6e8bd053c38cc9e
MatheusJanibelli <janibellim@gmail.com>
2021-05-12 16:31:44 -0300

    Adiciona calc_execucao.py

---
+++ conteudo/tempo-execução/calc_execucao.py
@@ -0,0 +1,25 @@
+import time
+
+
+class CalcExecucao:
+
+    def __init__(self, func: function) -> None:
+        self.func = func
+
+    def __call__(self, *args, **kwds) -> str:
+        inicio = time.time()
+        self.func(*args, *kwds)
+        final = time.time()
+
+        return f"Tempo de execução de {self.func.__name__}: {(final - inicio)}"
+
+
+if __name__ == "__main__":
+    @CalcExecucao
+    def teste(x, y):
+        for c in range(x):
+            if c % y == 0:
+                print(c)
+
+
+    print(teste(100, 2))
