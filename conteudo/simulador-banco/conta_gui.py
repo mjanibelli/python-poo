092001f324fda9c8e4375b7b834ae2dac04e5ddd
MatheusJanibelli <janibellim@gmail.com>
2021-06-03 12:15:50 -0300

    Adicionar GUI

---
+++ conteudo/simulador-banco/conta_gui.py
@@ -0,0 +1,73 @@
+import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import No, Tree
+
+import conta_modulo
+import janelas
+
+sg.theme("DarkGrey8")
+
+win_login = janelas.make_win_login()
+event, values = win_login.read()
+
+if event == "Inserir":
+    cliente_1 = conta_modulo.ContaBancaria(values["Nome"], values["SaldoIni"])
+    cliente_teste = conta_modulo.ContaBancaria("Teste", 100)
+    win_menu = janelas.make_win_menu()
+
+    while True:
+        event, values = win_menu.read()
+
+        if event in (None, "Sair"):
+            break
+        
+        elif event == "Sacar":
+            win_sacar = janelas.make_win_sacar()
+            event, values = win_sacar.read()
+
+            if event in (None, "Cancelar"):
+                win_sacar.close()
+            else:
+                quantia = float(values["SacaQnt"])
+                sg.popup(cliente_1.sacar(quantia))
+                win_sacar.close()
+
+        elif event == "Depositar":
+            win_depositar = janelas.make_win_depositar()
+            event, values = win_depositar.read()
+
+            if event in (None, "Cancelar"):
+                win_depositar.close()
+            else:
+                quantia = float(values["DeposQnt"])
+                sg.popup(cliente_1.depositar(quantia))
+                win_depositar.close()
+
+        elif event == "Transferência":
+            win_transf = janelas.make_win_transf()
+            event, values = win_transf.read()
+
+            if event in (None, "Cancelar"):
+                win_transf.close()
+            else:
+                quantia = float(values["TransfQnt"])
+                sg.popup(cliente_1.transferencia(cliente_teste, quantia))
+                win_transf.close()
+
+        elif event == "Abrir conta conjunta":
+            win_contaconj = janelas.make_win_contaconj()
+            event, values = win_contaconj.read()
+
+            if event in (None, "Cancelar"):
+                win_contaconj.close()
+            else:
+                nome_parc = values["NomeParc"]
+                saldo_inicial_parc = float(values["SaldoParc"])
+                cliente_2 = conta_modulo.ContaBancaria(nome_parc, saldo_inicial_parc)
+                cliente_conjunto = cliente_1 + cliente_2
+                sg.popup(cliente_conjunto)
+                win_contaconj.close()
+
+        elif event == "Minha conta":
+            sg.popup(cliente_1)
+    
+    win_menu.close()
---
+++ conteudo/simulador-banco/janelas.py
@@ -0,0 +1,56 @@
+from math import ceil
+from tkinter.constants import CENTER
+import PySimpleGUI as sg
+
+TITULO = "Conta Bancária"
+
+def make_win_login() -> sg.Window:
+    layout_login = [
+        [sg.Text("Nome: "), sg.Input(k="Nome")],
+        [sg.Text("Saldo Inicial: "), sg.Input(k="SaldoIni")],
+        [sg.Button("Inserir", bind_return_key=True), sg.Exit("Sair")]]
+
+    return sg.Window(TITULO, layout_login)
+
+
+def make_win_menu() -> sg.Window:
+    layout_menu = [
+        [sg.Text("Escolha uma opção:", justification=CENTER)], 
+        [sg.Button("Sacar"), sg.Button("Depositar"), sg.Button("Transferência")], 
+        [sg.Button("Abrir conta conjunta"), sg.Button("Minha conta")], 
+        [sg.Exit("Sair")]]
+
+    return sg.Window(TITULO, layout_menu)
+
+
+def make_win_sacar():
+    layout_sacar = [
+        [sg.Text("Digite a quantia que deseja sacar: "), sg.Input(k="SacaQnt")],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_sacar)
+
+
+def make_win_depositar():
+    layout_depositar = [
+        [sg.Text("Digite a quantia que deseja depositar: "), sg.Input(k="DeposQnt")],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_depositar)
+
+
+def make_win_transf():
+    layout_transf = [
+        [sg.Text("Digite a quantia que deseja transferir: "), sg.Input(k="TransfQnt")],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_transf)
+
+
+def make_win_contaconj():
+    layout_contaconj = [
+        [sg.Text("Digite o nome do seu parceiro: "), sg.Input(k="NomeParc")], 
+        [sg.Text("Digite o saldo inicial do seu parceiro: "), sg.Input(k="SaldoParc")],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_contaconj)
